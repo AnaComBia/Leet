e92c04752c58fa5b6b2146e9eff6ad658276f825
AnaComBia <anabeatrizetorres@gmail.com>
2021-09-01 00:01:04 -0300

    finaliza programa Leet

---
+++ Program.cs
@@ -0,0 +1,34 @@
+﻿using System;
+
+namespace Leet
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.Write("Frase que você quer traduzir para a linguagem leet Speak: ");
+            string tradução = Console.ReadLine()
+            .Replace("a", "4")
+            .Replace("A", "4")
+            .Replace("e", "3")
+            .Replace("E", "3")
+            .Replace("i", "1")
+            .Replace("I", "1")
+            .Replace("l", "1")
+            .Replace("t", "7")
+            .Replace("T", "7")
+            .Replace("s", "5")
+            .Replace("o", "0")
+            .Replace("O", "0")
+            .Replace("S", "5");
+
+            Console.Write("\nFrase em Leet Speak: ");
+            Console.WriteLine(tradução);
+
+
+
+
+            
+        }
+    }
+}
